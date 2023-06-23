f9e4616bf40bd2de8e144f5f7f4b7da89c2f964b
weiwei <342048733@qq.com>
2023-06-23 17:30:51 +0800

    easy API平台

---
+++ easyapi-frontend/config/defaultSettings.ts
@@ -0,0 +1,26 @@
+import { Settings as LayoutSettings } from '@ant-design/pro-components';
+
+/**
+ * @name
+ */
+const Settings: LayoutSettings & {
+  pwa?: boolean;
+  logo?: string;
+} = {
+  navTheme: 'light',
+  // 拂晓蓝
+  colorPrimary: '#1890ff',
+  // colorPrimary: "#1677FF",
+  layout: 'mix',
+  // layout: 'top',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  splitMenus: false,
+  title: 'EasyApi开放平台',
+  pwa: false,
+  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  iconfontUrl: '',
+};
+
+export default Settings;
